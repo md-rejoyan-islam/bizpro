4acceee541a4836955603ba48cb171aa6aa39c0b
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-03-31 00:21:42 +0600

    navbar added

---
+++ READme.md
@@ -0,0 +1,29 @@
+# Project Name
+
+## Description
+
+[Provide a brief description of your project]
+
+## Features
+
+- [List the main features of your project]
+
+## Installation
+
+[Provide instructions on how to install and set up your project]
+
+## Usage
+
+[Explain how to use your project, including any necessary commands or configurations]
+
+## Contributing
+
+[Specify how others can contribute to your project, if applicable]
+
+## License
+
+[Choose an appropriate license for your project and include it here]
+
+## Contact
+
+[Provide your contact information or any relevant links]
---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+const cross_btn = document.getElementById("cross-btn");
+const bar = document.getElementById("bar");
+const nav_screen = document.getElementsByClassName("nav-screen");
+const header = document.getElementsByTagName("header")[0];
+
+// toggle nav screen
+
+let isBarOpen = false;
+// toggle cross btn
+bar.addEventListener("click", () => {
+  nav_screen[0].style.display = "block";
+  header.style.position = "fixed";
+  header.style.width = "100%";
+  header.style.zIndex = "1000";
+  nav_screen[0].style.boxShadow = "0px 30px 50px 0px #03032557";
+  isBarOpen = true;
+});
+
+cross_btn.addEventListener("click", () => {
+  isBarOpen = false;
+  nav_screen[0].style.display = "none";
+  header.style.position = "relative";
+});
+console.log(document.body.offsetWidth);
+
+// if screen size change
+document.body.onresize = () => {
+  if (document.body.offsetWidth > 768) {
+    nav_screen[0].style.display = "flex";
+    nav_screen[0].style.boxShadow = "none";
+    header.style.position = "relative";
+  } else if (isBarOpen) {
+    nav_screen[0].style.display = "block";
+    header.style.position = "fixed";
+    nav_screen[0].style.boxShadow = "0px 30px 50px 0px #03032557";
+  } else {
+    nav_screen[0].style.display = "none";
+  }
+};
